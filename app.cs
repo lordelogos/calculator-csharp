74ba59e6329ff9a3706e741ab6d78a7edbb4ad5c
lordelogos <paulehiks@gmail.com>
2023-06-10 02:34:56 +0100

    feat: finished basic version of calculator

--- app.cs
+++ app.cs
@@ -7,7 +7,43 @@ namespace Calculator
     {
         public static void Main()
         {
-            Console.WriteLine("Hello world");
+            double input1, input2, result;
+            string operand;
+
+            Console.WriteLine("Welcome to my calculator v0.0.1");
+            Console.Write("Enter first number: ");
+            input1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            input2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What operation do you want to perform?");
+            Console.Write("a for Addition, m for Multiplication, d for Division, s for Subtraction: ");
+            operand = Console.ReadLine();
+
+            if (operand == "a")
+            {
+                result = Functions.Add(input1, input2);
+                Console.WriteLine("The answer is " + result);
+            }
+            else if (operand == "m")
+            {
+                result = Functions.Multiply(input1, input2);
+                Console.WriteLine("The answer is " + result);
+            }
+            else if (operand == "d")
+            {
+                result = Functions.Divide(input1, input2);
+                Console.WriteLine("The answer is " + result);
+            }
+            else if (operand == "s")
+            {
+                result = Functions.Subtract(input1, input2);
+                Console.WriteLine("The answer is " + result);
+            }
+            else
+            {
+                Console.WriteLine("You have selected an invalid operation.");
+            }
+
             Console.ReadLine();
         }
     };
