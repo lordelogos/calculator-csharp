2a311e6e3cdf044e2266e55207fbafac78d3db8c
lordelogos <paulehiks@gmail.com>
2023-06-10 02:07:04 +0100

    setup: initial project setup

---
+++ app.cs
@@ -0,0 +1,13 @@
+﻿// Making a calculator
+// fn: add, sub, mul, div
+// input: 2 numbers and operation
+namespace Calculator;
+
+class App
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+        Console.ReadLine();
+    }
+};
