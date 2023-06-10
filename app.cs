ddcc57c23accf7f356650948f7d606708d4d1a2b
lordelogos <paulehiks@gmail.com>
2023-06-10 02:48:24 +0100

    feat: updated calculator logic to allow multiple trials

--- app.cs
+++ app.cs
@@ -9,42 +9,53 @@ namespace Calculator
         {
             double input1, input2, result;
             string operand;
+            bool endProgram = true;
 
             Console.WriteLine("Welcome to my calculator v0.0.1");
-            Console.Write("Enter first number: ");
-            input1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter second number: ");
-            input2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("What operation do you want to perform?");
-            Console.Write("a for Addition, m for Multiplication, d for Division, s for Subtraction: ");
-            operand = Console.ReadLine();
 
-            if (operand == "a")
+            do
             {
-                result = Functions.Add(input1, input2);
-                Console.WriteLine("The answer is " + result);
-            }
-            else if (operand == "m")
-            {
-                result = Functions.Multiply(input1, input2);
-                Console.WriteLine("The answer is " + result);
-            }
-            else if (operand == "d")
-            {
-                result = Functions.Divide(input1, input2);
-                Console.WriteLine("The answer is " + result);
-            }
-            else if (operand == "s")
-            {
-                result = Functions.Subtract(input1, input2);
-                Console.WriteLine("The answer is " + result);
-            }
-            else
-            {
-                Console.WriteLine("You have selected an invalid operation.");
-            }
+                Console.Clear();
+                Console.Write("Enter first number: ");
+                input1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                input2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("What operation do you want to perform?");
+                Console.Write("a for Addition, m for Multiplication, d for Division, s for Subtraction: ");
+                operand = Console.ReadLine();
 
-            Console.ReadLine();
+                if (operand == "a")
+                {
+                    result = Functions.Add(input1, input2);
+                    Console.WriteLine("The answer is " + result);
+                }
+                else if (operand == "m")
+                {
+                    result = Functions.Multiply(input1, input2);
+                    Console.WriteLine("The answer is " + result);
+                }
+                else if (operand == "d")
+                {
+                    result = Functions.Divide(input1, input2);
+                    Console.WriteLine("The answer is " + result);
+                }
+                else if (operand == "s")
+                {
+                    result = Functions.Subtract(input1, input2);
+                    Console.WriteLine("The answer is " + result);
+                }
+                else
+                {
+                    Console.WriteLine("You have selected an invalid operation.");
+                }
+
+                Console.WriteLine("Do you want to continue?");
+                Console.Write("y to Continue program or any key to End program: ");
+                if (Console.ReadLine() != "y")
+                {
+                    endProgram = false;
+                }
+            } while (endProgram);
         }
     };
 }
